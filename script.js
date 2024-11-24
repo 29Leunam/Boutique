b27194718d1ed1895c7d89ad940cd34aef647c4e
29Leunam <waitjeje@gmail.com>
2024-11-24 01:11:14 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mostrar créditos
+function showCredits() {
+    document.getElementById('credits-modal').style.display = 'block';
+}
+
+// Cerrar créditos
+function closeCredits() {
+    document.getElementById('credits-modal').style.display = 'none';
+}
